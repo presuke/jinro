2f547f80491de4fceb4c39ea66ec37a73b27e285
renoneve <reno.neve@gmail.com>
2024-01-09 09:15:38 +0900

    modified:   resources/views/index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,17 +5,11 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-  <title>部屋作成</title>
-  @vite(['resources/css/app.css', 'resources/js/room/app.js'])
-  <link href="//fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
-  <link href="//cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
-  <link href="//cdn.jsdelivr.net/npm/vuetify@3.x/dist/vuetify.min.css" rel="stylesheet">
+  <title>test</title>
 </head>
 
 <body>
-  <div style="text-align:center;">
-    <div id="app" style="max-width:800px; margin: 0 auto;"></div>
-  </div>
+  test
 </body>
 
 </html>
