92932e7b4e7eca69aff2ac1e23c567fdc3ef0632
renoneve <reno.neve@gmail.com>
2024-01-17 23:57:37 +0900

    deleted:    public/index.html 	modified:   resources/views/index.blade.php 	modified:   vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,10 @@
-module.exports = {
-  pages: {
-    index: {
-      entry: "src/main.js",
-      title: "ああああ"
-    }
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+
+  pluginOptions: {
+    vuetify: {
+			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+		}
   }
-}
+})
