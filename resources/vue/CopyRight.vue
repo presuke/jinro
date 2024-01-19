81643bfedfb4bef79a9b2c67a834162ef0ccf28d
renoneve <reno.neve@gmail.com>
2024-01-19 19:07:23 +0900

    new file:   public/image/top2.png 	new file:   resources/image/logo1.png 	new file:   resources/image/top.png 	new file:   resources/vue/CopyRight.vue 	public/image/top.png 	resources/image/role/

---
+++ resources/vue/CopyRight.vue
@@ -0,0 +1,34 @@
+<template>
+    <h2>Copy Right</h2>
+    <ul style="margin-left:20px;">
+        <li>
+            キャラクターアイコン：<a href="https://blog.goo.ne.jp/akarise" target="_blank">ゆうひな様</a>
+        </li>
+        <li>
+            役割・背景イメージ：<a href="https://stablediffusionweb.com/" target="_blank">AI動画作成ツールStable Diffusion</a>
+        </li>
+        <li>
+            サウンドエフェクト：<a href="https://soundeffect-lab.info/" target="_blank">効果音ラボ様</a>
+        </li>
+        <li>
+            有償素材：<a href="https://pngtree.com/" target="_blank">PingTree様</a>
+        </li>
+    </ul>
+    <div style="font-size:smaller;">
+        ※素敵な素材、ツールのご提供に、感謝して使わせていただいております。
+    </div>
+    <h2>ソース</h2>
+    <div>
+        <img 
+        src="../image/github-mark.svg" 
+        class="icon" 
+        style="filter: drop-shadow(2px 2px 2px #66c);width:30px;height:30px;"
+        />
+        <a href="https://github.com/presuke/cashflow" target="_blank">
+            https://github.com/presuke/cashflow
+        </a>
+        <div style="font-size:smaller;">
+            ※vue+laravel勉強目的で作成したものです。非効率な記述や細かな不具合などあるかもしれません。
+        </div>
+    </div>
+</template>
