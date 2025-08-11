c1b55fe84869912ff4dabd7fce5420515d4e189b
renoneve <reno.neve@gmail.com>
2025-08-11 19:35:27 +0900

    modified:   resources/vue/play/App.vue

--- resources/vue/play/App.vue
+++ resources/vue/play/App.vue
@@ -1181,66 +1181,58 @@ export default {
 				hide-delimiter-background
 				>
 					<v-carousel-item
+					v-if="this.dialog.result.action.attackedPlayers.length > 0"
 					>
-					No1
+						<video ref="videoPlayer" playsinline autoplay muted :src="video.attack" class="video-element"></video>
+						以下のプレイヤーが人狼に拉致されて投獄されました。
+						<div style="clear:left;">
+							<div
+							style="float:left;text-align:center;"
+							v-for="player in this.dialog.result.action.attackedPlayers"
+							:key="player"
+							>
+								<img :src="rootPath + '/image/avatar/' + player.sex + '/icon' + player.img.toString().padStart( 2, '0') + '.png'"
+								class="icon overlay-image"
+								/>
+								<div>
+									{{ player.name }}
+								</div>
+
+							</div>
+						</div>
 					</v-carousel-item>
 					<v-carousel-item
+					v-if="this.dialog.result.action.freedomPlayers.length > 0"
 					>
-					No2
+						<video ref="videoPlayer" playsinline autoplay muted :src="video.rescue" class="video-element"></video>
+						以下のプレイヤーが天使により救われました。
+						<div style="clear:left;">
+							<div 
+							style="float:left;text-align:center;"
+							v-for="player in this.dialog.result.action.freedomPlayers"
+							:key="player"
+							>
+								<img :src="rootPath + '/image/avatar/' + player.sex + '/icon' + player.img.toString().padStart( 2, '0') + '.png'"
+								class="icon overlay-image"
+								/>
+								<div>
+									{{ player.name }}
+								</div>
+							</div>
+						</div>
 					</v-carousel-item>
 				</v-carousel>
 
-
-				<v-img
-				v-if="this.dialog.result.action.attackedPlayers.length > 0"
+				<ul 
+				style="margin-left:10px;list-style-type:none;"
+				v-for="message in this.dialog.result.action.message"
+				:key="message"
 				>
-					<video ref="videoPlayer" playsinline autoplay muted :src="video.attack" class="video-element"></video>
-					以下のプレイヤーが人狼に拉致されて投獄されました。
-					<div style="clear:left;">
-						<div
-						style="float:left;text-align:center;"
-						v-for="player in this.dialog.result.action.attackedPlayers"
-						:key="player"
-						>
-							<img :src="rootPath + '/image/avatar/' + player.sex + '/icon' + player.img.toString().padStart( 2, '0') + '.png'"
-							class="icon overlay-image"
-							/>
-							<div>
-								{{ player.name }}
-							</div>
+					<li>
+						{{ message }}
+					</li>
+				</ul>
 
-						</div>
-					</div>
-				</v-img>
-				<v-img
-				v-if="this.dialog.result.action.freedomPlayers.length > 0"
-				>
-					<video ref="videoPlayer" playsinline autoplay muted :src="video.rescue" class="video-element"></video>
-					以下のプレイヤーが天使により救われました。
-					<div style="clear:left;">
-						<div 
-						style="float:left;text-align:center;"
-						v-for="player in this.dialog.result.action.freedomPlayers"
-						:key="player"
-						>
-							<img :src="rootPath + '/image/avatar/' + player.sex + '/icon' + player.img.toString().padStart( 2, '0') + '.png'"
-							class="icon overlay-image"
-							/>
-							<div>
-								{{ player.name }}
-							</div>
-						</div>
-					</div>
-				</v-img>
-					<ul 
-					style="margin-left:10px;list-style-type:none;"
-					v-for="message in this.dialog.result.action.message"
-					:key="message"
-					>
-						<li>
-							{{ message }}
-						</li>
-					</ul>
 				<v-card-actions>
 					<v-spacer></v-spacer>
 					<v-btn
